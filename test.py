438494f2ca38f5d50211b2fc2f23eeb4ed63a2ab
Yufeng Wang <74530831+WYu-Feng@users.noreply.github.com>
2025-02-20 11:43:23 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,98 +1,97 @@
 import os
 import sys
 import argparse
-from data import create_dataset
 from data.universal_dataset import AlignedDataset_all
-from src.model import (ResidualDiffusion, Trainer, UnetResx2, set_seed)
+from src.model import (ResidualDiffusion, Trainer, UnetResx2)
 
 def parsr_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataroot", type=str, default='/mnt/Datasets/Restoration')
     parser.add_argument("--phase", type=str, default='test')
     parser.add_argument("--max_dataset_size", type=int, default=float("inf"))
-    parser.add_argument('--load_size', type=int, default=256, help='scale images to this size') #568
+    parser.add_argument('--load_size', type=int, default=268, help='scale images to this size')
     parser.add_argument('--crop_size', type=int, default=256, help='then crop to this size')
     parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
-    parser.add_argument('--preprocess', type=str, default='none', help='scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]')
-    parser.add_argument('--no_flip', type=bool, default=True, help='if specified, do not flip the images for data augmentation')
+    parser.add_argument('--preprocess', type=str, default='crop', help='scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]')
+    parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data augmentation')
     parser.add_argument("--bsize", type=int, default=2)
+    parser.add_argument("--mode", type=str, default='test')
     opt = parser.parse_args()
     return opt
 
-sys.stdout.flush()
-set_seed(10)
+if __name__=='__main__':
+    sys.stdout.flush()
 
-save_and_sample_every = 1000
-if len(sys.argv) > 1:
-    sampling_timesteps = int(sys.argv[1])
-else:
-    sampling_timesteps = 5
-train_num_steps = 100000
+    save_and_sample_every = 1000
+    if len(sys.argv) > 1:
+        sampling_timesteps = int(sys.argv[1])
+    else:
+        sampling_timesteps = 5
 
-condition = True
+    train_batch_size = 10
+    num_samples = 1
+    sum_scale = 0.01
+    image_size = 256
+    condition = True
+    opt = parsr_args()
 
-train_batch_size = 1
-num_samples = 1
-image_size = 256
+    results_folder = "./ckpt_universal/DSDM"
 
+    if 'universal' in results_folder:
+        dataset = AlignedDataset_all(opt, augment_flip=True, crop_patch=True)
+        dataset_test = AlignedDataset_all(opt, augment_flip=True, crop_patch=True, mode = 'test')
 
-opt = parsr_args()
+        num_unet = 1
+        objective = 'pred_res'
+        test_res_or_noise = "res"
+        train_num_steps = 300000
+        train_batch_size = 10
+        sum_scale = 0.01
+        delta_end = 1.8e-3
 
-results_folder = "./ckpt_universal"
+    MUNet = UnetResx2(
+        dim=64,
+        dim_mults=(1, 2, 4, 8),
+        num_unet=num_unet,
+        condition=condition,
+        objective=objective,
+        test_res_or_noise = test_res_or_noise
+    )
 
-dataset = AlignedDataset_all(opt, image_size, augment_flip=False, equalizeHist=True, crop_patch=False, generation=False, task='fog')
-num_unet = 1
-objective = 'pred_res'
-test_res_or_noise = "res"
-sampling_timesteps = 5
-sum_scale = 0.01
-ddim_sampling_eta = 0.
-delta_end = 1.8e-3
+    MUNet_diffusion = ResidualDiffusion(
+        MUNet,
+        image_size=image_size,
+        timesteps=1000,           # number of steps
+        delta_end = delta_end,
+        sampling_timesteps=sampling_timesteps,
+        objective=objective,
+        loss_type='l1',            # L1 or L2
+        condition=condition,
+        sum_scale=sum_scale,
+        test_res_or_noise = test_res_or_noise,
+    )
 
-model = UnetResx2(
-    dim=64,
-    dim_mults=(1, 2, 4, 8),
-    num_unet=num_unet,
-    condition=condition,
-    objective=objective,
-    test_res_or_noise = test_res_or_noise
-)
-diffusion = ResidualDiffusion(
-    model,
-    image_size=image_size,
-    timesteps=1000,           # number of steps
-    delta_end = delta_end,
-    sampling_timesteps=sampling_timesteps,
-    ddim_sampling_eta=ddim_sampling_eta,
-    objective=objective,
-    loss_type='l1',            # L1 or L2
-    condition=condition,
-    sum_scale=sum_scale,
-    test_res_or_noise = test_res_or_noise,
-)
+    trainer = Trainer(
+        MUNet_diffusion,
+        dataset_test,
+        opt,
+        train_batch_size=train_batch_size,
+        num_samples=num_samples,
+        train_lr=1e-4,
+        train_num_steps=train_num_steps,         # total training steps
+        gradient_accumulate_every=8,    # gradient accumulation steps
+        ema_decay=0.995,                # exponential moving average decay
+        amp=False,                        # turn on mixed precision
+        convert_image_to="RGB",
+        results_folder = results_folder,
+        condition=condition,
+        save_and_sample_every=save_and_sample_every
+    )
 
-trainer = Trainer(
-    diffusion,
-    dataset,
-    opt,
-    train_batch_size=train_batch_size,
-    num_samples=num_samples,
-    train_lr=2e-4,
-    train_num_steps=train_num_steps,         # total training steps
-    gradient_accumulate_every=2,    # gradient accumulation steps
-    ema_decay=0.995,                # exponential moving average decay
-    amp=False,                        # turn on mixed precision
-    convert_image_to="RGB",
-    results_folder = results_folder,
-    condition=condition,
-    save_and_sample_every=save_and_sample_every,
-    num_unet=num_unet,
-)
-
-# test
-# if not trainer.accelerator.is_local_main_process:
-#     pass
-# else:
-trainer.load(300)
-# trainer.set_results_folder('./result')
-trainer.test_fusion()
+    # train
+    path = 'your checkpoint path'
+    trainer.load(path)
+    # trainer.test_mu_net()
+    trainer.test_frn()
+    # trainer.train_frn()
+    # trainer.train_mu_net()
